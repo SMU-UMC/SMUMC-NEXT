a4024f2052d8e9fde81661ba3d572166eeb6c52b
김용민 <dydals3440@gmail.com>
2024-06-14 04:21:34 +0900

    style: eslint 수정

--- app/members/_components/active-member.tsx
+++ app/members/_components/active-member.tsx
@@ -2,12 +2,12 @@ import { MEMBERS } from '@/constants/members';
 import { MemberCard } from './member-card';
 
 export const ActiveMember = () => {
-	const CURRENT_MEMBERS = MEMBERS.filter((member) => member.year === 6);
-	return (
-		<div className='grid grid-cols-1 md:grid-cols-3 gap-20'>
-			{CURRENT_MEMBERS.map((member, idx) => {
-				return <MemberCard member={member} />;
-			})}
-		</div>
-	);
+  const CURRENT_MEMBERS = MEMBERS.filter(member => member.year === 6);
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-20">
+      {CURRENT_MEMBERS.map((member, idx) => {
+        return <MemberCard member={member} />;
+      })}
+    </div>
+  );
 };
--- app/members/_components/member-card.tsx
+++ app/members/_components/member-card.tsx
@@ -4,31 +4,31 @@ import { BiSolidCrown } from 'react-icons/bi';
 import Image from 'next/image';
 
 interface MemberCardProps {
-	member: Member;
+  member: Member;
 }
 
 export const MemberCard = ({ member }: MemberCardProps) => {
-	return (
-		<div className="flex flex-col items-center w-[200px] h-[300px] p-3 bg-[#E3E1E2] relative gap-10 rounded-2xl">
-			<div className="flex flex-col items-center justify-center mt-10 relative">
-				<Image
-					src={member.sex === 'w' ? '/images/woman.png' : '/images/man.png'}
-					alt={member.name}
-					width={105}
-					height={105}
-					className="object-cover border-white border rounded-full"
-				/>
-				{member.position !== '챌린저' && (
-					<BiSolidCrown color="#E6A912" className="absolute top-[-16px] " />
-				)}
-			</div>
-			<div className="">
-				<h3 className="mb-2 text-green-800 font-bold">{member.position}</h3>
-				<h3 className="font-bold text-slate-700 text-xl">{member.name}</h3>
-				<div className="text-green-800 text-sm">
-					{member.year}기 {member.part}
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="flex flex-col items-center w-[200px] h-[300px] p-3 bg-[#E3E1E2] relative gap-10 rounded-2xl">
+      <div className="flex flex-col items-center justify-center mt-10 relative">
+        <Image
+          src={member.sex === 'w' ? '/images/woman.png' : '/images/man.png'}
+          alt={member.name}
+          width={105}
+          height={105}
+          className="object-cover border-white border rounded-full"
+        />
+        {member.position !== '챌린저' && (
+          <BiSolidCrown color="#E6A912" className="absolute top-[-16px] " />
+        )}
+      </div>
+      <div className="">
+        <h3 className="mb-2 text-green-800 font-bold">{member.position}</h3>
+        <h3 className="font-bold text-slate-700 text-xl">{member.name}</h3>
+        <div className="text-green-800 text-sm">
+          {member.year}기 {member.part}
+        </div>
+      </div>
+    </div>
+  );
 };
