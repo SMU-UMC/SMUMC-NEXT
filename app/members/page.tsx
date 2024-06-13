91a2d4703f657e2315e5d9c0f022775f0683e4e7
김용민 <dydals3440@gmail.com>
2024-06-14 03:56:45 +0900

    feat: Suspense 적용

--- app/members/page.tsx
+++ app/members/page.tsx
@@ -1,10 +1,13 @@
+import { Suspense } from 'react';
 import { MembersFilter } from './_components/members-filter';
 
 const MembersPage = () => {
 	return (
 		<div className='min-h-full flex flex-col'>
 			<div className='flex flex-col items-center justify-center md:justify-start text-center flex-1 px-6 pb-20'>
-				<MembersFilter />
+				<Suspense fallback={<div>Loading...</div>}>
+					<MembersFilter />
+				</Suspense>
 			</div>
 		</div>
 	);
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,10 +1,13 @@
+import { Suspense } from 'react';
 import { FilterProjects } from './_components/filter-projects';
 
 const ProjectPage = () => {
 	return (
 		<div className='min-h-full flex flex-col'>
 			<div className='flex flex-col items-center justify-center md:justify-start text-center flex-1 px-6 pb-20'>
-				<FilterProjects />
+				<Suspense fallback={<div>Loading...</div>}>
+					<FilterProjects />
+				</Suspense>
 			</div>
 		</div>
 	);
