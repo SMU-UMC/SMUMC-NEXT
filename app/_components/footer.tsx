bba44e0dbb16a987f13bac1a8e3206c71fbfaad3
김용민 <dydals3440@gmail.com>
2024-06-14 06:10:37 +0900

    feat: Footer Contact 수정

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -1,8 +1,15 @@
+'use client';
+
 import React from 'react';
 import Logo from './logo';
 import { Button } from '@/components/ui/button';
 
+const contactEmail = 'dydals3440@gmail.com';
+
 export const Footer = () => {
+	const handleContactClick = () => {
+		window.location.href = `mailto:${contactEmail}`;
+	};
 	return (
 		<div className="flex items-center w-full p-6 bg-background z-50  dark:bg-[#1F1F1F]">
 			<Logo />
@@ -10,8 +17,8 @@ export const Footer = () => {
 				<Button variant="ghost" size="sm">
 					Privacy Policy
 				</Button>
-				<Button variant="ghost" size="sm">
-					Terms & Conditions
+				<Button variant="ghost" size="sm" onClick={handleContactClick}>
+					Contact To
 				</Button>
 			</div>
 		</div>
