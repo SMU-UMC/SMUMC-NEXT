1349e5203b58c9ce53a156486f322a7ae767c80b
hyewon <hijjoy04@gmail.com>
2025-08-30 16:44:57 +0900

    fix: 프로젝트 슬라이더 수정

---
+++ components/sections/home/project-carousel-slider.tsx
@@ -0,0 +1,48 @@
+'use client';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
+import { ProjectSlideCard } from '@/components/ui/project-slide-card';
+import { Project } from '@/types';
+
+interface ProjectCarouselSliderProps {
+	projects: Project[];
+	direction: 'left' | 'right';
+	keyPrefix: string;
+}
+
+export const ProjectCarouselSlider = ({
+	projects,
+	direction,
+	keyPrefix,
+}: ProjectCarouselSliderProps) => {
+	const isRightDirection = direction === 'right';
+
+	return (
+		<Swiper
+			autoplay={{ delay: 0, disableOnInteraction: false }}
+			modules={[Autoplay]}
+			loop
+			loopAdditionalSlides={1}
+			spaceBetween={8}
+			centeredSlides
+			slidesPerView={4}
+			speed={10000}
+			freeMode={true}
+			allowTouchMove={true}
+			grabCursor={true}
+			className={`!overflow-hidden w-full swiper-continuous-${direction}`}
+			dir={direction === 'left' ? 'ltr' : 'rtl'}
+		>
+			{[...projects, ...projects, ...projects].map((project, index) => (
+				<SwiperSlide
+					key={`${keyPrefix}-${index}`}
+					className="!w-[240px] swiper-slide"
+					dir={isRightDirection ? 'ltr' : undefined}
+				>
+					<ProjectSlideCard project={project} />
+				</SwiperSlide>
+			))}
+		</Swiper>
+	);
+};
